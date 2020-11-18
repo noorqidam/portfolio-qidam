343fd1f3d09892ff2a0b1d6197fc2a06d77ac9ba
noorqidam <noorqidam@gmail.com>
2020-11-18 21:29:18 +0700

    portfolio serverless

---
+++ actions/index.js
@@ -0,0 +1,21 @@
+import useSwr from "swr";
+
+const fetcher = (url) =>
+  fetch(url).then(async (res) => {
+    const result = await res.json();
+    if (res.status !== 200) {
+      return Promise.reject(result);
+    } else {
+      return result;
+    }
+  });
+
+export const useGetPosts = () => {
+  const { data, error, ...rest } = useSwr("/api/v1/posts", fetcher);
+  return { data, error, loading: !data && !error, ...rest };
+};
+
+export const useGetPostById = (id) => {
+  const { data, error, ...rest} = useSwr(id ? `/api/v1/posts/${id}` : null, fetcher)
+  return { data, error, loading: !data && !error, ...rest}
+}
---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import BaseLayout from '@/components/layouts/BaseLayout';
+import BasePage from '@/components/BasePage';
+
+const About = () => {
+  return (
+    <div>
+      <BaseLayout>
+        <BasePage>
+          <h1>I am About Page</h1>
+        </BasePage>
+      </BaseLayout>
+    </div>
+  )
+}
+
+export default About
