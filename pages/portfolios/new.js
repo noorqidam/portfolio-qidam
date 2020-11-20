7497e95f09a30d97805ffb1e010d3b7e51639ab1
noorqidam <noorqidam@gmail.com>
2020-11-20 14:49:57 +0700

    create portfolio

---
+++ pages/portfolios/new.js
@@ -0,0 +1,27 @@
+import BaseLayout from '@/components/layouts/BaseLayout';
+import BasePage from '@/components/BasePage';
+import withAuth from '@/hoc/withAuth';
+import { Row, Col } from 'reactstrap';
+import PortfolioForm from '@/components/PortfolioForm';
+import { createPortfolio } from '@/actions/portfolios';
+
+const PortfolioNew = ({user, loading: userLoading}) => {
+
+  const _createPortfolio = (data) => {
+    createPortfolio(data)
+  }
+
+  return (
+    <BaseLayout user={user} loading={userLoading}>
+      <BasePage header="Create Portfolio">
+        <Row>
+          <Col md="8">
+            <PortfolioForm onSubmit={_createPortfolio} />
+          </Col>
+        </Row>
+      </BasePage>
+    </BaseLayout>
+  )
+}
+
+export default withAuth(PortfolioNew)('admin');
